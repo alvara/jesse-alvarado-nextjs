0586d1303717ab49196563d4b6550e13c5bf1025
Jesse Alvarado <contact@jessealvarado.com>
2022-02-23 23:33:37 +0900

    create projection to get author name

--- web/pages/blog/[slug].js
+++ web/pages/blog/[slug].js
@@ -1,14 +1,42 @@
 // display individual blog post
-import { useRouter } from 'next/router'
+import client from '../../client'
 
-const Blog = () => {
-  const router = useRouter()
-  
+const Blog = (props) => {
+  const { title = 'Missing title', name = 'Missing name' } = props.post
   return (
     <article>
-      <h1>{router.query.slug}</h1>
+      <h1>{title}</h1>
+      <span>By {name}</span>
     </article>
   )
+}
+
+export async function getStaticPaths() {
+  const paths = await client.fetch(
+    `*[_type == "post" && defined(slug.current)][].slug.current`
+  )
+
+  return {
+    paths: paths.map((slug) => ({params: {slug}})),
+    fallback: true,
+  }
+}
+
+export async function getStaticProps(context) {
+  // It's important to default the slug so that it doesn't return "undefined"
+  const { slug = "" } = context.params
+  const post = await client.fetch(`
+    *[_type == "post" && slug.current == $slug][0]
+    {
+      title, 
+      "name": author->name
+    }
+  `, { slug })
+  return {
+    props: {
+      post
+    }
+  }
 }
 
 export default Blog
