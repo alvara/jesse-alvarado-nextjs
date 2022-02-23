d9ea174b6de5ca14a4f43214d4c843d5ef962478
Jesse Alvarado <contact@jessealvarado.com>
2022-02-24 01:34:59 +0900

    link to all blog posts on blog page

--- web/pages/blog.js
+++ web/pages/blog.js
@@ -1,10 +1,37 @@
-// blog.js
-const Blog = () => {
+import Link from 'next/link'
+import groq from 'groq'
+import client from '../client'
+
+// view all blog posts
+const Blog = ({posts}) => {
   return (
     <div>
       <p>Blog</p>
+
+      {posts.length > 0 && posts.map(
+          ({ _id, title = '', slug = '', publishedAt = '' }) =>
+            slug && (
+              <li key={_id}>
+                <Link href="/blog/[slug]" as={`/blog/${slug.current}`}>
+                  <a>{title}</a>
+                </Link>{' '}
+                ({new Date(publishedAt).toDateString()})
+              </li>
+            )
+        )}
     </div>
   )
+}
+
+export async function getStaticProps() {
+  const posts = await client.fetch(groq`
+    *[_type == "post" && publishedAt < now()] | order(publishedAt desc)
+  `)
+  return {
+    props: {
+      posts
+    }
+  }
 }
 
 export default Blog;
