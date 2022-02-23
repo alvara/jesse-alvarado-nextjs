21d9e4b38747765b08410852ef8170cec6ae6922
Jesse Alvarado <contact@jessealvarado.com>
2022-02-24 00:37:09 +0900

    add comment

--- web/utils/getSanityImgUrl.js
+++ web/utils/getSanityImgUrl.js
@@ -1,6 +1,7 @@
 import imageUrlBuilder from '@sanity/image-url'
 import client from '../client'
 
+// Get the image url from a Sanity query prop with image type
 export default function getSanityImgUrl(source) {
   return imageUrlBuilder(client).image(source)
 }
