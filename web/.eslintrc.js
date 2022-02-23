a393733e5af447e61510e0352fe288127a29e221
Jesse Alvarado <contact@jessealvarado.com>
2022-02-24 08:59:52 +0900

    update eslint rules for next js

--- web/.eslintrc.js
+++ web/.eslintrc.js
@@ -12,5 +12,9 @@ module.exports = {
     'object-curly-spacing': ['error', 'never'],
     'comma-dangle': 0,
     '@next/next/no-img-element': 0,
+     // suppress errors for missing 'import React' in files
+    "react/react-in-jsx-scope": "off",
+    // allow jsx syntax in js files (for next.js project)
+    "react/jsx-filename-extension": [1, {"extensions": [".js", ".jsx",".ts"]}] 
   },
 }
