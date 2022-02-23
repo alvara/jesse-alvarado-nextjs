964a509b0bd63dc7629f4821b7c02cb63cef04db
Jesse Alvarado <contact@jessealvarado.com>
2022-02-24 01:53:03 +0900

    add only first 3  blog post links to index

--- web/pages/index.js
+++ web/pages/index.js
@@ -1,10 +1,38 @@
+import Link from 'next/link'
+import groq from 'groq'
+import client from '../client'
+
 // index.js
-const Index = () => {
+const Index = ({posts}) => {
+
   return (
     <div>
       <p>Welcome!</p>
+
+      {posts.length > 0 && posts.map(
+          ({ _id, title = '', slug = '', publishedAt = '' }) =>
+            slug && (
+              <li key={_id}>
+                <Link href="/blog/[slug]" as={`/blog/${slug.current}`}>
+                  <a>{title}</a>
+                </Link>{' '}
+                ({new Date(publishedAt).toDateString()})
+              </li>
+            )
+        )}
     </div>
   )
+}
+
+export async function getStaticProps() {
+  const posts = await client.fetch(groq`
+    *[_type == "post" && publishedAt < now()][0..2] | order(publishedAt desc)
+  `)
+  return {
+    props: {
+      posts
+    }
+  }
 }
 
 export default Index;
