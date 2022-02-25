e08b9e55059d33af23eba71ef5f14420177299a1
Jesse Alvarado <contact@jessealvarado.com>
2022-02-25 16:53:41 +0900

    setup global styles

--- web/modules/sections/Navbar.tsx
+++ web/modules/sections/Navbar.tsx
@@ -1,7 +1,23 @@
+import Link from 'next/link'
 import React from 'react'
 
 export default function Navbar() {
   return (
-    <nav>navbar</nav>
+    <nav>
+      {/* logo */}
+      <div>Jesse Alvarado</div>
+
+      {/* menu */}
+      <div>
+        <Link href="/#about"><a>About</a></Link>
+        <Link href="/#portfolio"><a>Portfolio</a></Link>
+      </div>
+
+      {/* socials */}
+      <div>
+      <Link href="/#about"><a>Github</a></Link>
+      <Link href="/#about"><a>LinkedIn</a></Link>
+      </div>
+    </nav>
   )
 }
