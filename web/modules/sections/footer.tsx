365eeac7f2076b404f9f66294235b9a9184ab34b
Jesse Alvarado <contact@jessealvarado.com>
2022-02-25 12:48:05 +0900

    removed rename conflict files

--- web/modules/sections/footer.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function footer() {
-  return (
-    <footer>footer goes here</footer>
-  )
-}
--- web/modules/sections/navbar.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function navbar() {
-  return (
-    <nav>Navbar goes here</nav>
-  )
-}
