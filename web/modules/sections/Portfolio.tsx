b6a461944504f1dc8816939c609e4e1e75849d25
Jesse Alvarado <contact@jessealvarado.com>
2022-02-26 21:05:23 +0900

    prepare portfolio card component

--- web/modules/sections/Portfolio.tsx
+++ web/modules/sections/Portfolio.tsx
@@ -1,15 +1,21 @@
 import React from 'react'
-import InfoCard from '../widgets/InfoCard'
+import PortfolioCard from '../widgets/PortfolioCard'
 
 export default function Portfolio() {
   return (
     <div id="portfolio" className="h-100 text-center d-flex flex-column justify-content-center">
-    <h1>Some <strong>projects</strong>  I&apos;ve worked on</h1>
+    <h1><strong>Projects</strong>  I&apos;ve worked on</h1>
     <div className="row">
-      <div className="col-md-3"><InfoCard /></div>
-      <div className="col-md-3"><InfoCard /></div>
-      <div className="col-md-3"><InfoCard /></div>
-      <div className="col-md-3"><InfoCard /></div>
+      <div className="col-md-3"><PortfolioCard /></div>
+      <div className="col-md-3"><PortfolioCard /></div>
+      <div className="col-md-3"><PortfolioCard /></div>
+      <div className="col-md-3"><PortfolioCard /></div>
+      <div className="col-md-3"><PortfolioCard /></div>
+      <div className="col-md-3"><PortfolioCard /></div>
+      <div className="col-md-3"><PortfolioCard /></div>
+      <div className="col-md-3"><PortfolioCard /></div>
+      <div className="col-md-3"><PortfolioCard /></div>
+      <div className="col-md-3"><PortfolioCard /></div>
     </div>
   </div>
   )
--- web/modules/widgets/PortfolioCard.tsx
+++ web/modules/widgets/PortfolioCard.tsx
@@ -1,11 +1,11 @@
 import Image from 'next/image'
 import React from 'react'
 
-export default function InfoCard() {
+export default function portfolioCard() {
   return (
-    <div className="infoCard text-start m-3">
+    <div className="portfolioCard text-start m-3">
       <Image src="https://via.placeholder.com/300x300" width="250" height="250" layout="responsive" alt="test" className="mb-4"/>
-      <h6>Result Oriented</h6>
+      <h6>Portfolio Oriented</h6>
       <p>I focus on the end result first, building an MVP with prioritized features and an iterative approach.</p>
     </div>
   )
