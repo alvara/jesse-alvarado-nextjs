fd4a8d8bb488a34ab6eace64893791d6e2cc3bb2
Jesse Alvarado <contact@jessealvarado.com>
2022-02-26 14:20:40 +0900

    add placeholder image for structuring layout

--- web/modules/sections/HeroHeader.tsx
+++ web/modules/sections/HeroHeader.tsx
@@ -1,7 +1,10 @@
+import Image from 'next/image'
 import React from 'react'
 
 export default function HeroHeader() {
   return (
-    <div>Hero Header</div>
+    <div>
+      <Image src="https://via.placeholder.com/350x150" layout="fixed" width="350" height="150" />
+    </div>
   )
 }
