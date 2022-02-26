d9526ed39c0de8e921b8c85ba934c7b601a8d6b6
Jesse Alvarado <contact@jessealvarado.com>
2022-02-26 18:34:47 +0900

    remove unnecessary classes

--- web/modules/sections/HeroHeader.tsx
+++ web/modules/sections/HeroHeader.tsx
@@ -7,7 +7,7 @@ export default function HeroHeader() {
       <div className="col-md-12">
         <h1>Hi, Im Jesse</h1>
        <p>I build modern web applications with React, NextJS and Netlify.</p>
-        <div className="mx-auto d-flex justify-content-center mt-5" >
+        <div className="d-flex justify-content-center mt-5" >
           <Image src="https://via.placeholder.com/350x150" layout="fixed" width="350" height="400" alt="test"/>
         </div>
       </div>
--- web/pages/index.tsx
+++ web/pages/index.tsx
@@ -17,9 +17,9 @@ export default function Index({posts}) {
   
   return (
     <>
-      <Container wrapperClass="vh-100-w-nav" className="h-100 h-100 text-center d-flex flex-column justify-content-end"><HeroHeader /></Container>
-      <Container wrapperClass="min-h-100 d-flex justify-content-center align-items-center" className=""><Philosophy /></Container>
-      {/* <Container wrapperClass="vh-100" className="h-100"><Portfolio /></Container> */}
+      <Container wrapperClass="vh-100-w-nav" className="h-100 text-center d-flex flex-column justify-content-end"><HeroHeader /></Container>
+      <Container wrapperClass="min-h-100 d-flex align-items-center"><Philosophy /></Container>
+      <Container wrapperClass="min-h-100 d-flex justify-content-center align-items-center"><Portfolio /></Container>
       {/* <Container wrapperClass="vh-100" className="h-100"><LatestPosts posts={posts} /></Container> */}
       {/* <Container wrapperClass="vh-100" className="h-100"><MySkills /></Container> */}
       {/* <Container wrapperClass="vh-100" className="h-100"><ContactMe /></Container> */}
