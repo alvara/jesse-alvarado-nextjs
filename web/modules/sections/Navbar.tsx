4b8ffcc4229a0bc163ecae742845cb9e0d42bbae
Jesse Alvarado <contact@jessealvarado.com>
2022-02-26 12:15:39 +0900

    setup bootstrap navbar

--- web/modules/sections/Navbar.tsx
+++ web/modules/sections/Navbar.tsx
@@ -3,22 +3,52 @@ import React from 'react'
 
 export default function Navbar() {
   return (
-    <nav>
-      {/* logo */}
-      <Link href="/">Jesse Alvarado</Link>
-
-      {/* menu */}
-      <div>
-        <Link href="/#about"><a>About</a></Link>
-        <Link href="/#portfolio"><a>Portfolio</a></Link>
-        <Link href="/blog"><a>Blog</a></Link>
-      </div>
-
-      {/* socials */}
-      <div>
-      <Link href="/#about"><a>Github</a></Link>
-      <Link href="/#about"><a>LinkedIn</a></Link>
-      </div>
-    </nav>
+    <nav className="navbar navbar-expand-md navbar-dark bg-primary">
+    <div className="container-fluid">
+     <Link href="/"><a className="navbar-brand">Jesse Alvarado</a></Link>
+        <button className="navbar-toggler ms-auto" type="button" data-bs-toggle="collapse" data-bs-target="#collapseNavbar">
+            <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="navbar-collapse collapse" id="collapseNavbar">
+            <ul className="navbar-nav ms-auto">
+                <li className="nav-item active">
+                    <Link href="/#about"><a className="nav-link">About</a></Link>
+                </li>
+                <li className="nav-item">
+                    <Link href="/#portfolio"><a className="nav-link">Portfolio</a></Link>
+                </li>
+                <li className="nav-item">
+                    <Link href="/blog"><a className="nav-link">Blog</a></Link>
+                </li>
+            </ul>
+            <ul className="navbar-nav ms-auto">
+                <li className="nav-item">
+                    <Link href="/#about"><a className="nav-link">Github</a></Link>
+                </li>
+                <li className="nav-item">
+                    <Link href="/#about"><a className="nav-link">LinkedIn</a></Link>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
   )
 }
+
+    // <nav>
+    //   {/* logo */}
+    //   <Link href="/">Jesse Alvarado</Link>
+
+    //   {/* menu */}
+    //   <div>
+    //     
+    //     
+    //     
+    //   </div>
+
+    //   {/* socials */}
+    //   <div>
+    //   
+    //   
+    //   </div>
+    // </nav>
