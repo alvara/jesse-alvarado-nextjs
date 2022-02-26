ae61ba1629ae21f40f342d13992f85a9f0e4a43b
Jesse Alvarado <contact@jessealvarado.com>
2022-02-26 14:35:15 +0900

    enable boostrap grid and columns

--- web/modules/sections/HeroHeader.tsx
+++ web/modules/sections/HeroHeader.tsx
@@ -3,8 +3,12 @@ import React from 'react'
 
 export default function HeroHeader() {
   return (
-    <div>
-      <Image src="https://via.placeholder.com/350x150" layout="fixed" width="350" height="150" />
+    <div className="row">
+      <div className="col-sm-8">col-sm-8</div>
+      <div className="col-sm-4">
+        <Image src="https://via.placeholder.com/350x150" layout="fixed" width="350" height="150" />
+
+      </div>
     </div>
   )
 }
