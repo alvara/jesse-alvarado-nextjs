22dbd4ac236949d07abf596928b490767344240e
Jesse Alvarado <contact@jessealvarado.com>
2022-02-28 16:15:38 +0900

    update copywrite

--- web/modules/sections/HeroHeader.tsx
+++ web/modules/sections/HeroHeader.tsx
@@ -6,7 +6,7 @@ export default function HeroHeader() {
     <div className="row">
       <div className="col-md-12">
         <h1>Hi, Im Jesse</h1>
-       <p>I build modern web applications and MVPs that are optimized in design and performance.</p>
+       <p>I build modern web applications and MVPs that are optimized in design and performance to help people.</p>
         <div className="d-flex justify-content-center mt-5" >
           <Image src="/personal-pic.png" layout="fixed" width="350" height="500vh" sizes="50vh" alt="test"/>
         </div>
--- web/modules/sections/Philosophy.tsx
+++ web/modules/sections/Philosophy.tsx
@@ -8,25 +8,25 @@ export default function Philosophy() {
         <div className="col-md-3">
           <InfoCard 
             title='Result Oriented'
-            content='I focus on the end result first, building an MVP with prioritized features and an iterative approach.'
+            content='I focus on the end result first, building prioritized features with an iterative approach to quickly deliver a MVP.'
           />
         </div>
         <div className="col-md-3">
           <InfoCard 
-            title='Result Oriented'
-            content='I focus on the end result first, building an MVP with prioritized features and an iterative approach.'
+            title='Team Player'
+            content='I use clear communication, well documented code, and and an open mindset to make working with teams a productive experience.'
           />
         </div>
         <div className="col-md-3">
           <InfoCard 
-            title='Result Oriented'
-            content='I focus on the end result first, building an MVP with prioritized features and an iterative approach.'
+            title='Clean Design'
+            content='I am passionate about creating beautiful web apps that users can easily understand and enjoy.'
           />
         </div>
         <div className="col-md-3">
           <InfoCard 
-            title='Result Oriented'
-            content='I focus on the end result first, building an MVP with prioritized features and an iterative approach.'
+            title='Modern Stack'
+            content='I am never afraid to utilize new frameworks and tools to take advantage of the latest advancements in technology.'
           />
         </div>
       </div>
