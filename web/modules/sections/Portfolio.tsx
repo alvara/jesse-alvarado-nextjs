bca62ee15afd2ae4911c35194d55d2a93ea3b0eb
Jesse Alvarado <contact@jessealvarado.com>
2022-02-28 20:25:21 +0900

    implement working toggle between pill clicks

--- web/modules/sections/Portfolio.tsx
+++ web/modules/sections/Portfolio.tsx
@@ -2,8 +2,6 @@ import React, {useState} from 'react'
 import PropTypes from 'prop-types'
 
 import PortfolioCard from '../widgets/PortfolioCard'
-import Pill from '../../common/Pill'
-
 export default function Portfolio({portfolio, tags}) {
 
   // TODO: Move featured tag to the front of the array
@@ -12,8 +10,6 @@ export default function Portfolio({portfolio, tags}) {
 
   const [selected, setSelected] = useState("featured")
 
-
-
   return (
     <div id="portfolio" className="h-100 text-center d-flex flex-column justify-content-center">
     <h1><strong>Projects</strong>  I&apos;ve worked on</h1>
@@ -21,7 +17,7 @@ export default function Portfolio({portfolio, tags}) {
       <div className='col'>
           {tags.length > 0 && tags.map(
             ({title, slug, _id}) => (
-              <Pill key={_id} title={title} active={selected === slug}  />
+              <span onClick={()=>setSelected(slug)} key={_id} className={`pill ${selected === slug ? 'active' : ''}`} >{title}</span>
             )
           )}
       </div>
