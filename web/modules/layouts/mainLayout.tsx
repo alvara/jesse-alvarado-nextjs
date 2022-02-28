eec98d777f6ef76502378ae29086f7f6216cc4e7
Jesse Alvarado <contact@jessealvarado.com>
2022-02-28 13:05:12 +0900

    adjust color shades

--- web/modules/layouts/mainLayout.tsx
+++ web/modules/layouts/mainLayout.tsx
@@ -14,7 +14,7 @@ export default function MainLayout({children}) {
       </Head>
       <Navbar/>
       <main>{children}</main>
-      <Container wrapperClass="bg-dark text-white p-3"><Footer /></Container>
+      <Container wrapperClass="bg-dark text-white pt-3 pb-0"><Footer /></Container>
     </>
   )
 }
