e1db8a9522f439c1db42b7dfc893c3b14916e76d
Jesse Alvarado <contact@jessealvarado.com>
2022-03-01 16:10:14 +0900

    create dynamic page for portfolio single

---
+++ web/pages/portfolio/[slug].tsx
@@ -0,0 +1,122 @@
+// display individual blog post
+import {ReactElement} from 'react'
+import groq from 'groq'
+import {PortableText} from '@portabletext/react'
+import PropTypes from "prop-types"
+import Head from 'next/head'
+
+import MainLayout from "../../modules/layouts/mainLayout"
+import client from '../../client'
+import getSanityImgUrl from '../../utils/getSanityImgUrl'
+
+// Configure Portable Text
+const ptComponents = {
+  types: {
+    figure: ({value}) => {
+      if (!value?.asset?._ref) {
+        return null
+      }
+      return (
+        <img
+          alt={value.alt || ' '}
+          loading="lazy"
+          src={getSanityImgUrl(value).width(320).height(240).fit('max').auto('format').url()}
+          />
+          )
+        }
+      }
+}
+
+// Declare Portfolio Item Component
+const PortfolioItem = (props) => {
+  const post = props.post
+  return (
+    <>
+      <Head>
+        {post?.title && (
+         <title>{post.title}</title>
+        )}
+
+        {/* TODO: add description to schema and insert here for each post */}
+      </Head>
+      <article>
+        <h1>{post?.title}</h1>
+        <div>By {post?.author}</div>
+
+      {post?.categories && (
+        <ul>
+          {post?.categories.map(category => <li key={category}>{category}</li>)}
+        </ul>
+      )}
+
+      {post?.authorImg && (
+        <div>
+          <img src={getSanityImgUrl(post?.authorImg).width(150).url()} alt='author'/>
+        </div>
+      )}
+
+
+      {post?.content && (
+        <PortableText
+          value={post.content}
+          components={ptComponents}
+        />
+        )}
+      </article>
+    </>
+  )
+}
+
+// Load Page in Layout
+PortfolioItem.getLayout = function getLayout(page: ReactElement) {
+  return (
+    <MainLayout>
+      {page}
+    </MainLayout>
+  )
+}
+
+// Define Proptypes for PortfolioItem Component
+PortfolioItem.propTypes = {
+  post: PropTypes.object,
+  title: PropTypes.string,
+  author: PropTypes.string,
+  authorImg: PropTypes.string,
+  categories: PropTypes.arrayOf(PropTypes.string),
+  content: PropTypes.arrayOf(PropTypes.object)
+};
+
+// Query for current portfolio item
+const query = groq`*[_type == "portfolio" && slug.current == $slug][0]{
+  title,
+  "author": author->name,
+  "categories": categories[]->title,
+  "authorImg": author->image,
+  content
+}`
+
+// Define path for SSG pages '/portfolio/[slug]'.
+export async function getStaticPaths() {
+  const paths = await client.fetch(
+    `*[_type == "post" && defined(slug.current)][].slug.current`
+  )
+
+  return {
+    paths: paths.map((slug) => ({params: {slug}})),
+    fallback: true,
+  }
+}
+
+// 
+export async function getStaticProps(context) {
+  // default empty slug so that it doesn't return "undefined"
+  const {slug = ""} = context.params
+  const post = await client.fetch(query, {slug})
+  return {
+    props: {
+      post
+    }
+  }
+}
+
+export default PortfolioItem
