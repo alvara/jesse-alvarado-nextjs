025e365da3490e8adf2f577014873c3780b2a56c
Jesse Alvarado <contact@jessealvarado.com>
2022-03-01 15:06:50 +0900

    add philosophy section images

--- web/modules/sections/Philosophy.tsx
+++ web/modules/sections/Philosophy.tsx
@@ -9,25 +9,29 @@ export default function Philosophy() {
           <InfoCard 
             title='Result Oriented'
             content='I focus on the end result first, building prioritized features with an iterative approach to quickly deliver a MVP.'
+            mainImage='/result-oriented.jpg'
           />
         </div>
         <div className="col-md-3">
           <InfoCard 
             title='Team Player'
             content='I use clear communication, well documented code, and and an open mindset to make working with teams a productive experience.'
+            mainImage='/team-player.jpg'
           />
         </div>
         <div className="col-md-3">
           <InfoCard 
             title='Clean Design'
             content='I am passionate about creating beautiful web apps that users can easily understand and enjoy.'
+            mainImage='/clean-design.jpg'
           />
         </div>
         <div className="col-md-3">
           <InfoCard 
             title='Modern Stack'
             content='I am never afraid to utilize new frameworks and tools to take advantage of the latest advancements in technology.'
-          />
+            mainImage='/modern-stack.jpg'
+         />
         </div>
       </div>
   )
