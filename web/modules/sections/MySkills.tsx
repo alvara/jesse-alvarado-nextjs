9aad5509b41b1f79e75a3a90727e15f90a3bc54f
Jesse Alvarado <contact@jessealvarado.com>
2022-03-02 12:41:37 +0900

    add table to skill section

--- web/modules/sections/MySkills.tsx
+++ web/modules/sections/MySkills.tsx
@@ -26,28 +26,41 @@ export default function MySkills({tags}) {
           <p>I'm a self-driven, team-oriented full stack developer with a background in web development.</p><p>I aim to solve problems and drive results for clients by developing web applications that are fast in performance, flexible in design, and typed in clean code for easy collaboration and maintenance.</p>
           <p>I am currently focused on learning and applying Go(Golang) and Google Cloud Platform (GCP) services to projects that are built on NextJS.</p>
           <div className='row'>
-            <div className='col-md-3'>Work Experience</div>
-            <div className='col-md-9 mb-3'>Full Stack Developer<br/>Beams Inc. (2016-2021)</div>
-            <div className='col-md-3'>Education</div>
-            <div className='col-md-9 mb-3'>BA in Management Information Systems<br/>Western Washington University</div>
-            <div className='col-md-3'>Certifications</div>
-            <div className='col-md-9 mb-3'>
-              Ruby on Rails Bootcamp 2021<br /> 
-              Le Wagon - Tokyo (2021)<br/>
-              <Link href="/lewagon-certificate.pdf">Certificate</Link>
-              <br /><br />
+            <div className='col-12'>
+              <div className="table-responsive">
+              <table className="table caption-top">
+                <caption>History</caption>
+                <tbody>
+                  <tr>
+                    <th scope="row">Work Experience</th>
+                    <td>Full Stack Developer<br/>Beams Inc., Tokyo (2016-2021)</td>
+                  </tr>
+                  <tr>
+                    <th scope="row">Education</th>
+                    <td>BA Mgmt. Information Systems<br/>Western Washington University</td>
+                  </tr>
+                  <tr>
+                    <th scope="row">Certifications</th>
+                    <td>Ruby on Rails Bootcamp 2021<br /> 
+                      Le Wagon - Tokyo (2021)<br/>
+                      <Link href="/lewagon-certificate.pdf">Certificate</Link>
+                      <br /><br />
 
-            Google Cloud Essentials<br/>
-            <Link href="https://www.cloudskillsboost.google/public_profiles/e6ee1b1f-fdc4-4ef4-a99f-95176b203d30">Google Skill badge</Link><br/><br/>
+                      Google Cloud Essentials<br/>
+                      <Link href="https://www.cloudskillsboost.google/public_profiles/e6ee1b1f-fdc4-4ef4-a99f-95176b203d30">Google Skill badge</Link><br/><br/>
 
-            Serverless Firebase Development<br/>
-            <Link href="https://www.cloudskillsboost.google/public_profiles/e6ee1b1f-fdc4-4ef4-a99f-95176b203d30">Google Skill badge</Link>            
-            </div>      
+                      Serverless Firebase Development<br/>
+                      <Link href="https://www.cloudskillsboost.google/public_profiles/e6ee1b1f-fdc4-4ef4-a99f-95176b203d30">Google Skill badge</Link>            
+                    </td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+            </div>     
           </div>
         </div>
       </div>
     </div>
-
   )
 }
 
