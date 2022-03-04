628bcfb2a69d5b0c3c028522818c8bf397f3cfa6
Jesse Alvarado <contact@jessealvarado.com>
2022-03-04 10:36:45 +0900

    remove crossorigin causing deployment issue

--- web/pages/_document.tsx
+++ web/pages/_document.tsx
@@ -9,7 +9,7 @@ export default function Document() {
       <Head>
         {/* Google Font */}
         <link rel="preconnect" href="https://fonts.googleapis.com"></link>
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin></link>
+        <link rel="preconnect" href="https://fonts.gstatic.com"></link>
         <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300&family=Work+Sans:wght@300;500&display=swap" rel="stylesheet"></link>
 
         
