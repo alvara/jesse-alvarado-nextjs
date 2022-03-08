e6bbc9affef4a1af03d47dbf818461fe43c3731b
Jesse Alvarado <contact@jessealvarado.com>
2022-03-08 09:09:54 +0900

    make intro with hello instead of hi for more formal

--- web/pages/index.tsx
+++ web/pages/index.tsx
@@ -20,7 +20,7 @@ export default function Index({posts, portfolio, tags, showcaseTags}) {
       <Container wrapperClass="vh-100-w-nav pb-0" className="h-100 text-center d-flex flex-column justify-content-end">
         <HeroHeader 
           img={'/220308-portrait.png'} 
-          preTitle={'Hi, I\'m Jesse'}
+          preTitle={'Hello, I\'m Jesse'}
           title={'I build modern web applications to empower businesses.'}
         />
       </Container>
