6d20500e46fba19d7a1cd25a023249ea0d92aaa1
Jesse Alvarado <contact@jessealvarado.com>
2022-03-23 23:37:17 +0900

    change pretitle to be WGAC AAA compliant

--- web/pages/_document.tsx
+++ web/pages/_document.tsx
@@ -2,10 +2,11 @@
 // pages/_document.js
 
 import {Html, Head, Main, NextScript} from 'next/document'
+import Script from 'next/script'
 
 export default function Document() {
   return (
-    <Html>
+    <Html lang="en">
       <Head>
         {/* Google Font */}
         {/* <link rel="preconnect" href="https://fonts.googleapis.com"></link>
@@ -15,11 +16,13 @@ export default function Document() {
         <link rel="preconnect" href="https://fonts.googleapis.com"></link>
         <link rel="preconnect" href="https://fonts.gstatic.com" ></link>
         <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@100;200;400&display=swap" rel="stylesheet"></link>
-    
+  
       </Head>
       <body>
         <Main />
         <NextScript />
+    
+        
       </body>
     </Html>
   )
