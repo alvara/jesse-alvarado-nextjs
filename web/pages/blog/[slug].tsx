8a476dcff7c1e4662da86092f0a9d5996aa159d8
Jesse Alvarado <contact@jessealvarado.com>
2022-04-01 17:06:08 +0900

    remove 100vh

--- web/pages/blog/[slug].tsx
+++ web/pages/blog/[slug].tsx
@@ -2,15 +2,14 @@
 import {ReactElement} from 'react'
 import groq from 'groq'
 import {PortableText} from '@portabletext/react'
-import PropTypes from "prop-types"
+import PropTypes from 'prop-types'
 
-import MainLayout from "../../modules/layouts/mainLayout"
+import MainLayout from '../../modules/layouts/mainLayout'
 import client from '../../client'
 import getSanityImgUrl from '../../utils/getSanityImgUrl'
 import Head from 'next/head'
 import Container from '../../common/Container'
 import HeroHeader from '../../modules/sections/HeroHeader'
-
 
 const ptComponents = {
   types: {
@@ -24,72 +23,61 @@ const ptComponents = {
           loading="lazy"
           src={getSanityImgUrl(value).width(320).height(240).fit('max').auto('format').url()}
         />
-          )
-        }
-      }
+      )
+    },
+  },
 }
 
 const Post = (props) => {
   const post = props.post
   return (
     <>
+      <Head>
+        {post?.title && <title>{post.title}</title>}
 
-    <Head>
-      {post?.title && (
-       <title>{post.title}</title>
-      )}
+        {/* TODO: add description to schema and insert here for each post */}
+      </Head>
 
-      {/* TODO: add description to schema and insert here for each post */}
-    </Head>
+      <Container
+        wrapperClass=""
+        className="h-100 d-flex align-items-center justify-content-center text-center"
+      >
+        <HeroHeader
+          img={post?.mainImage}
+          imgType="blog"
+          title={post?.title}
+          subtitle={post?.summary}
+          date={post?.publishedAt}
+          tags={post?.tags}
+        />
+      </Container>
 
-    <Container wrapperClass="vh-100-w-nav" className="h-100 d-flex align-items-center justify-content-center text-center">
-      <HeroHeader 
-        img={post?.mainImage} 
-        imgType='blog'
-        title={post?.title}
-        subtitle={post?.summary}
-        date={post?.publishedAt}
-        tags={post?.tags}
-      />
-    </Container>
-
-    <Container wrapperClass='portableText'>
-      <div className="row">
-        <div className="mx-auto col-md-8 px-4">
-        {post?.content && (
-          <PortableText
-          value={post.content}
-          components={ptComponents}
-          />
-          )}
+      <Container wrapperClass="portableText">
+        <div className="row">
+          <div className="mx-auto col-md-8 px-4">
+            {post?.content && <PortableText value={post.content} components={ptComponents} />}
+          </div>
         </div>
-      </div>
-    </Container>
-  </>
+      </Container>
+    </>
   )
 }
 
 // Load Page in Layout
 Post.getLayout = function getLayout(page: ReactElement) {
-  return (
-    <MainLayout>
-      {page}
-    </MainLayout>
-  )
+  return <MainLayout>{page}</MainLayout>
 }
 
 // Define Proptypes for Post Component
 Post.propTypes = {
   post: PropTypes.object,
   title: PropTypes.string,
-  content: PropTypes.arrayOf(PropTypes.object)
-};
+  content: PropTypes.arrayOf(PropTypes.object),
+}
 
 // Define path for SSG pages '/blog/[slug]'.
 export async function getStaticPaths() {
-  const paths = await client.fetch(
-    `*[_type == "post" && defined(slug.current)][].slug.current`
-  )
+  const paths = await client.fetch(`*[_type == "post" && defined(slug.current)][].slug.current`)
 
   return {
     paths: paths.map((slug) => ({params: {slug}})),
@@ -111,12 +99,12 @@ const query = groq`*[_type == "post" && slug.current == $slug][0]{
 
 export async function getStaticProps(context) {
   // default empty slug so that it doesn't return "undefined"
-  const {slug = ""} = context.params
+  const {slug = ''} = context.params
   const post = await client.fetch(query, {slug})
   return {
     props: {
-      post
-    }
+      post,
+    },
   }
 }
 
--- web/pages/portfolio/[slug].tsx
+++ web/pages/portfolio/[slug].tsx
@@ -2,10 +2,10 @@
 import {ReactElement} from 'react'
 import groq from 'groq'
 import {PortableText} from '@portabletext/react'
-import PropTypes from "prop-types"
+import PropTypes from 'prop-types'
 import Head from 'next/head'
 
-import MainLayout from "../../modules/layouts/mainLayout"
+import MainLayout from '../../modules/layouts/mainLayout'
 import client from '../../client'
 import getSanityImgUrl from '../../utils/getSanityImgUrl'
 import Container from '../../common/Container'
@@ -23,10 +23,10 @@ const ptComponents = {
           alt={value.alt || ' '}
           loading="lazy"
           src={getSanityImgUrl(value).width(320).height(240).fit('max').auto('format').url()}
-          />
-          )
-        }
-      }
+        />
+      )
+    },
+  },
 }
 
 // Declare Portfolio Item Component
@@ -34,34 +34,29 @@ const PortfolioItem = (props) => {
   const post = props.post
   return (
     <>
-
       <Head>
-        {post?.title && (
-         <title>{post.title}</title>
-        )}
+        {post?.title && <title>{post.title}</title>}
 
         {/* TODO: add description to schema and insert here for each post */}
       </Head>
 
-      <Container wrapperClass="vh-100-w-nav" className="h-100 d-flex align-items-center justify-content-center text-center px-4">
-        <HeroHeader 
-          img={post?.mainImage} 
-          imgType='hero'
+      <Container
+        wrapperClass=""
+        className="d-flex align-items-center justify-content-center text-center px-4"
+      >
+        <HeroHeader
+          img={post?.mainImage}
+          imgType="hero"
           title={post?.title}
           subtitle={post?.summary}
           tags={post?.tags}
         />
       </Container>
 
-      <Container wrapperClass='portableText'>
+      <Container wrapperClass="portableText">
         <div className="row">
           <div className="col-12 offset-sm-1 col-sm-10 offset-md-2 col-md-8 px-4">
-          {post?.content && (
-            <PortableText
-            value={post.content}
-            components={ptComponents}
-            />
-            )}
+            {post?.content && <PortableText value={post.content} components={ptComponents} />}
           </div>
         </div>
       </Container>
@@ -71,19 +66,15 @@ const PortfolioItem = (props) => {
 
 // Load Page in Layout
 PortfolioItem.getLayout = function getLayout(page: ReactElement) {
-  return (
-    <MainLayout>
-      {page}
-    </MainLayout>
-  )
+  return <MainLayout>{page}</MainLayout>
 }
 
 // Define Proptypes for PortfolioItem Component
 PortfolioItem.propTypes = {
   post: PropTypes.object,
   title: PropTypes.string,
-  content: PropTypes.arrayOf(PropTypes.object)
-};
+  content: PropTypes.arrayOf(PropTypes.object),
+}
 
 // Query for current portfolio item
 const query = groq`*[_type == "portfolio" && slug.current == $slug][0]{
@@ -110,15 +101,15 @@ export async function getStaticPaths() {
   }
 }
 
-// 
+//
 export async function getStaticProps(context) {
   // default empty slug so that it doesn't return "undefined"
-  const {slug = ""} = context.params
+  const {slug = ''} = context.params
   const post = await client.fetch(query, {slug})
   return {
     props: {
-      post
-    }
+      post,
+    },
   }
 }
 
