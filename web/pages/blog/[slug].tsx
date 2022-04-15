d0fd0090c6e92c4770d2af95553dd487417f3333
Jesse Alvarado <contact@jessealvarado.com>
2022-04-15 20:12:49 +0900

    style index bg

--- web/pages/blog/[slug].tsx
+++ web/pages/blog/[slug].tsx
@@ -39,7 +39,7 @@ const Post = (props) => {
       </Head>
 
       <Container
-        wrapperClass=""
+        wrapperClass="index-header"
         className="h-100 d-flex align-items-center justify-content-center text-center"
       >
         <Header
