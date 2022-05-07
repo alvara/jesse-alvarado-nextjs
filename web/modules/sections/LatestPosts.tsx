b106ff3af4971d91a79d231edcdccbaa16449dbd
Jesse Alvarado <contact@jessealvarado.com>
2022-05-07 23:58:42 +0900

    reformat code with prettier

--- web/modules/sections/LatestPosts.tsx
+++ web/modules/sections/LatestPosts.tsx
@@ -1,38 +1,38 @@
 import React from 'react'
-import PropTypes from "prop-types"
+import PropTypes from 'prop-types'
 import InfoCard from '../widgets/InfoCard'
 import Link from 'next/link'
 
 export default function LatestPosts({posts}) {
   return (
-    <div className='text-center'>
-      <span className='preTitle'>Blog</span>
+    <div className="text-center">
+      <span className="preTitle">Blog</span>
       <h2>Latest Articles</h2>
-      <div className='row'>
-      {posts.length > 0 && posts.map(
-        ({_id, title = '', slug, publishedAt = '', mainImage, categories = []}) =>
-           (
-            <div className='col-lg-4'  key={_id} >
-              <InfoCard 
-                className='postCard'
+      <div className="row">
+        {posts.length > 0 &&
+          posts.map(({_id, title = '', slug, publishedAt = '', mainImage, categories = []}) => (
+            <div className="col-lg-4" key={_id}>
+              <InfoCard
+                className="postCard"
                 img={mainImage}
-                imgType='blog'
-                href={"/blog/[slug]"}
-                as={`/blog/${slug.current}`} 
-                title={title} 
+                imgType="blog"
+                href={'/blog/[slug]'}
+                as={`/blog/${slug.current}`}
+                title={title}
                 subtitle={new Date(publishedAt).toDateString()}
                 tags={categories}
                 height={120}
               />
             </div>
-          )
-        )}
+          ))}
       </div>
-      <Link href={'/blog'}><button className='btn mt-5'>Read More Articles</button></Link>
+      <Link href={'/blog'}>
+        <button className="btn mt-5">Read More Articles</button>
+      </Link>
     </div>
   )
 }
 
 LatestPosts.propTypes = {
-  posts : PropTypes.arrayOf(PropTypes.object)
+  posts: PropTypes.arrayOf(PropTypes.object),
 }
